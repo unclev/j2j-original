c985e879c2ac771c3f7b5efe37a0002d4507425e
binary <binary@01e182ee-9b2a-0410-97fe-e04f53beed18>
2007-03-08 17:55:07 +0000

    SVN versions control
    git-svn-id: svn://svn.jrudevels.org/j2j/trunk@68 01e182ee-9b2a-0410-97fe-e04f53beed18

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import j2j
 from twisted.words.protocols.jabber import component
 from twisted.internet import reactor
 from config import config
-from twisted.python import versions
 
-version=versions.Version("j2j",1,1,5)
+__all__=['j2j','client','database','roster','utils','adhoc']
+revision=0
 
-c=j2j.j2jComponent(reactor,version.package+' version '+version.base())
+for modName in __all__:
+    module=__import__(modName,globals(),locals())
+    try:
+        modRev=int(module.__id__.split(" ")[2])
+    except:
+        modRev=0
+    if modRev>revision:
+        revision=modRev
+
+if revision==0:
+    revision=''
+else:
+    revision='.r'+str(revision)
+
+version="1.1.5"+revision
+
+c=j2j.j2jComponent(reactor,"J2J-Twisted version "+version)
 f=component.componentFactory(config.JID,config.PASSWORD)
 connector = component.buildServiceManager(config.JID, config.PASSWORD, "tcp:%s:%s" % (config.HOST, config.PORT))
 c.setServiceParent(connector)
