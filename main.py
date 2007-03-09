5537a8d4170207b5e91872cb60f5c08ba5fe6421
binary <binary@01e182ee-9b2a-0410-97fe-e04f53beed18>
2007-03-09 10:32:49 +0000

    git-svn-id: svn://svn.jrudevels.org/j2j/trunk@72 01e182ee-9b2a-0410-97fe-e04f53beed18

--- main.py
+++ main.py
@@ -5,33 +5,46 @@ from config import config
 
 __all__=['j2j','client','database','roster','utils','adhoc']
 revision=0
+date=0
 
 __id__="$Id$"
 try:
     modRev=int(__id__.split(" ")[2])
+    modDate=int(__id__.split(" ")[3].replace("-",""))
 except:
     modRev=0
+    modDate=0
 
 if modRev>revision:
     revision=modRev
+if modDate>date:
+    date=modDate
 
 for modName in __all__:
     module=__import__(modName,globals(),locals())
     try:
         modRev=int(module.__id__.split(" ")[2])
+        modDate=int(module.__id__.split(" ")[2].replace("-",""))
     except:
         modRev=0
+        modDate=0
     if modRev>revision:
         revision=modRev
+    if modDate>revision:
+        date=modDate
 
 if revision==0:
     revision=''
 else:
     revision='.r'+str(revision)
+if date!=0:
+    date=str(date)
+    print date
+    revision=revision+" %s-%s-%s" % (date[:4],date[4:6],date[6:8])
 
 version="1.1.6"+revision
 
-c=j2j.j2jComponent(reactor,"J2J-Twisted version "+version)
+c=j2j.j2jComponent(reactor,version)
 f=component.componentFactory(config.JID,config.PASSWORD)
 connector = component.buildServiceManager(config.JID, config.PASSWORD, "tcp:%s:%s" % (config.HOST, config.PORT))
 c.setServiceParent(connector)
